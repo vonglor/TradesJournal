79fa39da419e62b5c903cda4bf03f36359dc94a6
vonglor patou <vonglorpatou@vonglors-Laptop.local>
2026-07-11 11:16:07 +0700

    json

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 const api = axios.create({
     baseURL: 'https://trades-journal.onrender.com/api',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
 });
 export default api;
